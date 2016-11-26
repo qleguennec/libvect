7795f3d35f6443f29136c4595af309d8f87a1f50
Quentin Le Guennec <quentin.leguennec1@gmail.com>
2016-11-26 17:05:04 +0000

    fix vect_init

--- vect_init.c
+++ vect_init.c
@@ -6,7 +6,7 @@
 /*   By: qle-guen <qle-guen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/06/10 16:04:40 by qle-guen          #+#    #+#             */
-/*   Updated: 2016/11/14 14:55:24 by qle-guen         ###   ########.fr       */
+/*   Updated: 2016/11/26 17:04:38 by qle-guen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,9 @@
 #include "../libft/malloc.h"
 #include "../libft/libft.h"
 
-t_vect			*vect_init
-	(void *data, size_t size)
+void	vect_init(t_vect *v)
 {
-	t_vect		*ret;
-
-	ret = ft_memalloc(sizeof(*ret));
-	ret->total = size;
-	MALLOC(ret->data, size);
-	ret->used = size;
-	ft_memcpy(ret->data, data, size);
-	return (ret);
+	v->data = NULL;
+	v->used = 0;
+	v->total = 0;
 }
