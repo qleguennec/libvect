00c5a3f4ee331b61363b3d4a528e230dee89a027
Quentin Le Guennec <quentin.leguennec1@gmail.com>
2017-02-06 10:42:46 +0100

    fix norm

--- vect_memmem.c
+++ vect_memmem.c
@@ -6,7 +6,7 @@
 /*   By: qle-guen <qle-guen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/23 01:50:30 by qle-guen          #+#    #+#             */
-/*   Updated: 2016/12/23 01:56:57 by qle-guen         ###   ########.fr       */
+/*   Updated: 2017/02/06 10:42:45 by qle-guen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 void	*vect_memmem(t_vect *a, unsigned char *s, size_t n)
 {
-	size_t			i;
-	unsigned char *s1;
+	size_t				i;
+	unsigned char		*s1;
 
 	s1 = a->data;
 	while ((void *)s1 + n < a->data + a->used)
